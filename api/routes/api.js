2bbbc776d2bd6f982f9e443ba368158c005c1e21
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-12-07 10:25:44 +0100

    added authentication middleware and comments

--- api/routes/api.js
+++ api/routes/api.js
@@ -2,17 +2,41 @@ const express = require('express');
 const passport = require('passport');
 
 const router = express.Router();
-const authenticated = () => passport.authenticate('jwt', { session: false });
 
-router.get('/public', (req, res) => {
-  res.send({ message: 'Hey this is a public message!' })
+/**
+ * authenticationRequired is a middleware that use the jwt strategy to authenticate
+ * the use. If authentication fails, passport will respond with a 401 Unauthorized status.
+ * If authentication succeeds, the `req.user` property will be set to the authenticated user.
+ */
+const authenticationRequired = passport.authenticate('jwt', { session: false });
+
+/**
+ * authentication middleware overrides the default behavior of passport. The next handler is
+ * always invoked. If authentication fails, the `req.user` property will be set to null.
+ * If authentication succeeds, the `req.user` property will be set to the authenticated user.
+ * see: http://www.passportjs.org/docs/authenticate/#custom-callback
+ */
+const authentication = (req, res, next) => {
+  return passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) { next(err) }
+    req.user = user || null;
+    next();
+  })(req, res, next);
+}
+
+// This endpoint is accessible by authenticated and anonymous users
+router.get('/public', authentication, (req, res) => {
+  const username = req.user ? req.user.username : 'anonymous';
+  res.send({ message: `Hello ${username}, this message is public!` })
 })
 
-router.get('/private', authenticated(), (req, res) => {
-  res.send({ message: 'Hey this is a private message!' });
+// This endpoint is protected and has access to the authenticated user.
+router.get('/private', authenticationRequired, (req, res) => {
+  res.send({ message: `Hello ${req.user.username}, only logged in users can see this message!` })
 })
 
-router.get('/me', authenticated(), (req, res) => {
+// This endpoint is protected and has access to the authenticated user.
+router.get('/me', authenticationRequired, (req, res) => {
   res.send({ user: req.user });
 })
 
